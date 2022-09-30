fa75a3f684d28b9e833d3def43f5948a52b5688f
seonyeonghun2 <kjca1117@naver.com>
2022-09-30 16:52:11 +0900

    final update : xhr vs ajax

--- app.js
+++ app.js
@@ -1,59 +1,70 @@
 // 사용할 모듈(=자바스크립트 파일)을 기록하는 곳
-const express = require('express'); // express 모듈을 express 객체를 사용한다.
-const morgan = require('morgan'); // HTTP request logger를 사용한다.
-const bodyParser = require('body-parser'); // body-parser를 사용한다.
+const express = require("express"); // express 모듈을 express 객체를 사용한다.
+const morgan = require("morgan"); // HTTP request logger를 사용한다.
+const bodyParser = require("body-parser"); // body-parser를 사용한다.
 
 const app = express(); // app이라는 express 객체의 인스턴스를 생성한다.
 
-app.set('view engine', 'pug'); // view 템플릿 엔진을 pug를 사용한다고 설정, views 폴더에 pug파일 생성
-app.set('port', 3000); // 서버 포트를 3000으로 사용한다고 설정
-
+app.set("view engine", "pug"); // view 템플릿 엔진을 pug를 사용한다고 설정, views 폴더에 pug파일 생성
+app.set("port", 3000); // 서버 포트를 3000으로 사용한다고 설정
 
 // 사용할 미들웨어 모듈 : 미들웨어는 순서대로 호출됨(=순서가 중요함)
-app.use(express.static('public')); // 정적인 파일 : img, css, js 처리
-app.use(bodyParser.json());  // json 데이터를 처리
-app.use(bodyParser.urlencoded({extended: true})); // 인코딩된 문자열 처리
+app.use(express.static("public")); // 정적인 파일 : img, css, js 처리
+app.use(bodyParser.json()); // json 데이터를 처리
+app.use(bodyParser.urlencoded({ extended: true })); // 인코딩된 문자열 처리
 app.use(morgan("dev")); // 개발 끝나고, 서비스(=배포)할때는 combined 사용 [정보의 양이 적고 많음의 차이]
 
 // routing 처리 : 요청에 따른 응답 방법을 결정하는 것
 // routing method : .get(), .post(), .put(), .del(), .patch()...expressjs.con > 라우팅 안내서 참고!!
 
-app.get('/', function (req, res) { // get 요청이 들어오면 응답
-    //res.send('Hello World'); // 서버에서 text로 응답
-    //res.sendFile(__dirname+"/public/main.html"); // HTML 파일로 응답
-    res.render('index', { title: 'hanul tour!', message: '지금 예약하세요!' }); // pug로 응답
+app.get("/", function (req, res) {
+  // get 요청이 들어오면 응답
+  //res.send('Hello World'); // 서버에서 text로 응답
+  //res.sendFile(__dirname+"/public/main.html"); // HTML 파일로 응답
+  res.render("index", { title: "hanul tour!", message: "지금 예약하세요!" }); // pug로 응답
 });
 
-app.get('/tour', function (req, res) { // get 요청이 들어오면 응답
-    //res.sendFile(__dirname+"/public/reserve.html"); // HTML 파일로 응답
-    res.render('tour');
+app.get("/tour", function (req, res) {
+  // get 요청이 들어오면 응답
+  //res.sendFile(__dirname+"/public/reserve.html"); // HTML 파일로 응답
+  res.render("tour");
 });
 
-app.get('/reserve', function (req, res) { // get 요청이 들어오면 응답
-    //res.sendFile(__dirname+"/public/reserve.html"); // HTML 파일로 응답
-    res.render('reserve');
+app.get("/reserve", function (req, res) {
+  // get 요청이 들어오면 응답
+  //res.sendFile(__dirname+"/public/reserve.html"); // HTML 파일로 응답
+  res.render("reserve");
 });
 
-app.get('/customer', function (req, res) { // get 요청이 들어오면 응답
-    //res.sendFile(__dirname+"/public/reserve.html"); // HTML 파일로 응답
-    res.render('cs');
+app.get("/customer", function (req, res) {
+  // get 요청이 들어오면 응답
+  //res.sendFile(__dirname+"/public/reserve.html"); // HTML 파일로 응답
+  res.render("cs");
 });
 
-app.post('/send_reserve', function(req, res){
-    //res.send(req.body.guest_name+"님, 환영합니다."); // bodyParser가 이해하고 출력
-    console.log(req.body.guest_name, req.body.guest_phone)
-    // if(req.body.guest_name === "홍길동") {
-    //     res.send(req.body.guest_name+"님, 환영합니다.")
-    // } else {
-    //     res.send("회원가입 페이지로 이동합니다")
-    // }
-    // 실제 서비스로직상 DB와 연동해서 회원정보 유무를 확인하고,
-    // 이에 따른 처리를 실시해야 함!
-})
+app.post("/send_post", function (req, res) {
+  // 자바스크립트 xhr을 이용한 post 데이터가 전송되면, 이를 출력
+  console.log("수신 데이터 : ", req.body);
+  console.log("수신 데이터 타입 : ", typeof req.body);
+  res.send("<h1>POST 데이터가 정상적으로 수신되었습니다.</h1>");
+});
 
+app.post("/send_post_ajax", function (req, res) {
+  // 실제 서비스로직상 DB와 연동해서 회원정보 유무를 확인하고,
+  // 이에 따른 처리를 실시해야 함!
+  console.log("수신 데이터 : ", req.body);
+  console.log("수신 데이터 타입 : ", typeof req.body);
 
-const CURRENT_PORT = app.get('port'); // app.set() 으로 4000번을 지정했음.
+  // 클라이언트에게 전송할 데이터
+  const responseData = {
+    guest_name: req.body.name,
+    guest_phone: req.body.phone,
+  };
+  res.json(responseData); // json 타입으로 응답함
+});
 
-app.listen(CURRENT_PORT, function(){
-    console.log(`서버가 ${CURRENT_PORT}번 포트에서 실행중입니다`)
-})
+const CURRENT_PORT = app.get("port"); // app.set() 으로 4000번을 지정했음.
+
+app.listen(CURRENT_PORT, function () {
+  console.log(`서버가 ${CURRENT_PORT}번 포트에서 실행중입니다`);
+});
--- public/script/ajax.js
+++ public/script/ajax.js
@@ -1,26 +1,41 @@
-$(function(){
-    // 예약확인 버튼 클릭하면
-    $("#checkForm").submit(function(e){        
-        e.preventDefault(); // form이 submit을 발생
-        // ajax로 통신 - 교재 p.501 참고
-        const query = {
-            guest_name: $("#guest_name").val(),
-            guest_phone: $("#guest_phone").val()
-        }
-        $.ajax({
-            url: "/send_reserve",
-            method: "post",
-            data: query, // jquery.com 참고
-            //dataType: "json",
-            success: function(data){
-                alert("post 전송 성공!, 메인페이지로 이동합니다.");
-                location.href="/"
-            },
-            error: function(data){
-                alert("에러발생");
-                console.log(data);
-            }
-        })
-    })
-})
+$(function () {
+  // 확인 버튼 클릭하면 (js)XMLHttpRequest 객체를 이용해 비동기 통신
+  document.querySelector("#checkForm").addEventListener("submit", function () {
+    const query = {
+      guest_name: document.querySelector("#guest_name").value,
+      guest_phone: document.querySelector("#guest_phone").value,
+    };
 
+    send_post_data_by_XHR("/send_post", query);
+  });
+  function send_post_data_by_XHR(url, data) {
+    const xhr = new XMLHttpRequest();
+    xhr.open("post", url);
+    xhr.setRequestHeader("Content-Type", "application/json"); // 요청헤더를 json으로
+    xhr.send(JSON.stringify(data));
+  }
+  // ajax_send 버튼 클릭으로 post 방식의 데이터 전송 (update)
+  $("#ajax_send").click(function () {
+    // ajax로 통신 - 교재 p.501 참고
+    const ajax_data = {
+      name: $("#guest_name").val(),
+      phone: $("#guest_phone").val(),
+    };
+    $.ajax({
+      url: "/send_post_ajax",
+      method: "post",
+      data: ajax_data, // jquery.com 참고
+      //dataType: "json",
+      success: function (responseData) {
+        console.log("post 전송 성공!");
+        console.log(typeof responseData, responseData); // 요청 성공에 따른
+        $(".ajax_result").html(
+          `<h2>${responseData.guest_name}님, 연락처는 ${responseData.guest_phone}</h2>`
+        );
+      },
+      error: function () {
+        console.log("post 전송 에러!");
+      },
+    });
+  });
+});
